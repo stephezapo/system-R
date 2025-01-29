8b59d5574df913273874c7b19e67d532b188922f
zapotocky <zapotocky@zactrack.com>
2025-01-29 17:31:54 +0100

    adapted the color theme

--- system_r/ui/src/main/java/org/stephezapo/system_r/core/ui/ColorTheme.java
+++ system_r/ui/src/main/java/org/stephezapo/system_r/core/ui/ColorTheme.java
@@ -4,10 +4,10 @@ import javafx.scene.paint.Color;
 
 public class ColorTheme
 {
-    public static final Color COLOR_LIGHT_GRAY = Color.valueOf("#dddddd");
+    public static final Color COLOR_LIGHT_GRAY = Color.valueOf("#848585");
     public static final Color COLOR_ACCENT = Color.valueOf("#c678dd");
-    public static final Color COLOR_FOREGROUND = Color.valueOf("#abb2bf");
-    public static final Color COLOR_TILE_HEADER = Color.valueOf("#5c6370");
-    public static final Color COLOR_TILE_BACKGROUND = Color.valueOf("#32363e");
-    public static final Color COLOR_MAIN_BACKGROUND = Color.valueOf("#282c34");
+    public static final Color COLOR_FOREGROUND = Color.valueOf("#848585");
+    public static final Color COLOR_TILE_HEADER = Color.valueOf("#43454A");
+    public static final Color COLOR_TILE_BACKGROUND = Color.valueOf("#2B2D30");
+    public static final Color COLOR_MAIN_BACKGROUND = Color.valueOf("#1E1F22");
 }
